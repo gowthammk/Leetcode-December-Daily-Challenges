189613577ed432979eebdad0856df089433d47eb
Gowtham <mkgowtham7@gmail.com>
2020-12-17 12:32:54 +0000

    4Sum II

--- Day 17.py
+++ Day 17.py
@@ -1,82 +1,42 @@
-# http://andybromberg.com/sentiment-analysis-python/
-# Andy Bromberg's Simple Sentiment Analysis System
-# Uses data from Pang & Lee (2005)
-# Uses a Naive Bayes Classifier Train the System
-#  NB Updated 2016 for package changes around scores
+#  4Sum II
+#
+# Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l)
+# there are such that A[i] + B[j] + C[k] + D[l] is zero.
+#
+# To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500.
+# All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+#
+# Example:
+#
+# Input:
+# A = [ 1, 2]
+# B = [-2,-1]
+# C = [-1, 2]
+# D = [ 0, 2]
+#
+# Output:
+# 2
+#
+# Explanation:
+# The two tuples are:
+# 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+# 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+#
 
-import re, math, collections, itertools, sys, os
-import nltk, nltk.classify.util, nltk.metrics
-from nltk.classify import NaiveBayesClassifier
-from nltk.metrics import BigramAssocMeasures, scores
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.probability import FreqDist, ConditionalFreqDist
-
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-
-def evaluate_features(feature_select):
-    # reading pre-labeled input and splitting into lines
-    negSentences = open(os.path.join(__location__, 'rt-polarity-neg.txt'), 'r', encoding='utf8')
-    posSentences = open(os.path.join(__location__, 'rt-polarity-pos.txt'), 'r', encoding='utf8')
-    negSentences = re.split(r'\n', negSentences.read())
-    posSentences = re.split(r'\n', posSentences.read())
-
-    stop_words = set(stopwords.words('english'))
-    posFeatures = []
-    negFeatures = []
-    # breaks up the sentences into lists of individual words
-    # creates instance structures for classifier
-
-
-    for i in posSentences:
-            ps = []
-            posWords = re.findall(r"[\w']+|[.,!?;]", i)
-            for w in posWords:
-                if w not in stop_words:
-                    ps.append(w)
-            posWords = [feature_select(ps), 'pos']
-            posFeatures.append(posWords)
-    for i in negSentences:
-            ns = []
-            negWords = re.findall(r"[\w']+|[.,!?;]", i)
-            for w in negWords:
-                if w not in stop_words:
-                    ns.append(w)
-            negWords = [feature_select(ns), 'neg']
-            negFeatures.append(negWords)
-
-    posCutoff = int(math.floor(len(posFeatures) * 3 / 4))
-    negCutoff = int(math.floor(len(negFeatures) * 3 / 4))
-    trainFeatures = posFeatures[:posCutoff] + negFeatures[:negCutoff]
-    testFeatures = posFeatures[posCutoff:] + negFeatures[negCutoff:]
-
-
-    # Runs the classifier on the testFeatures
-    classifier = NaiveBayesClassifier.train(trainFeatures)
-
-    # Sets up labels to look at output
-    referenceSets = collections.defaultdict(set)
-    testSets = collections.defaultdict(set)
-    for i, (features, label) in enumerate(testFeatures):  # enumerate adds number-count to each item
-        referenceSets[label].add(i)  # recorded polarity for these test sentences
-        predicted = classifier.classify(features)  # classifiers' proposed polarity for tests
-        testSets[predicted].add(i)
-
-    # Outputs
-    print('train on %s instances, test on %s instances' % (len(trainFeatures), len(testFeatures)))
-    print('accuracy:', nltk.classify.util.accuracy(classifier, testFeatures))
-    print('pos precision:', scores.precision(referenceSets['pos'], testSets['pos']))
-    print('pos recall:', scores.recall(referenceSets['pos'], testSets['pos']))
-    print('neg precision:', scores.precision(referenceSets['neg'], testSets['neg']))
-    print('neg recall:', scores.recall(referenceSets['neg'], testSets['neg']))
-    classifier.show_most_informative_features(10)
-
-
-def make_full_dict(words):
-    return dict([(word, True) for word in words])
-
-print('using all words as features')
-evaluate_features(make_full_dict)
-
+class Solution:
+    def fourSumCount(self, A, B, C, D):
+        res = {}
+        for i in A:
+            for j in B:
+                sum_ = i + j
+                if sum_ not in res:
+                    res[sum_] = 1
+                else:
+                    res[sum_] += 1
+        count = 0
+        for i in C:
+            for j in D:
+                if 0 - j - i in res:
+                    print(res[0 - i - j])
+                    count += res[0 - i - j]
+        return count
